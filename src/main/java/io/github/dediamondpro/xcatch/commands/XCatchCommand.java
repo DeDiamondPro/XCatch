3fcee15ffe0c665c0b5a863df516fd336037907e
DeDiamondPro <67508414+DeDiamondPro@users.noreply.github.com>
2022-04-08 16:25:13 +0200

    1.0

--- src/main/java/io/github/dediamondpro/xcatch/commands/XCatchCommand.java
+++ src/main/java/io/github/dediamondpro/xcatch/commands/XCatchCommand.java
@@ -35,9 +35,11 @@ public class XCatchCommand implements CommandExecutor {
             sender.sendMessage(new String[]
                     {
                             "§8[§cXCatch§8] version " + XCatch.INSTANCE.getDescription().getVersion(),
+                            "§7/xcatch help, show this help menu.",
                             "§7/xcatch view [<player>], view a player's recent flags and/or bans.",
-                            "§7/xcatch debug <player>, give debug statistics of a player.",
-                            "§7/xcatch reload, reload XCatch's config."
+                            "§7/xcatch info, get some statics about XCatch on your server.",
+                            "§7/xcatch reload, reload XCatch's config.",
+                            "§7/xcatch debug <player>, give debug statistics of a player."
                     });
             return true;
         }
